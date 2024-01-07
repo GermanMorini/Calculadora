b6887331f4d9ff4c9227e14e015262f12784b809
Germán Morini <germorini25@gmail.com>
2024-01-07 20:23:51 -0300

    Añadida librería para mejorar la apariencia

--- src/main/java/german/calculadora1/MainApplication.java
+++ src/main/java/german/calculadora1/MainApplication.java
@@ -1,5 +1,6 @@
 package german.calculadora1;
 
+import atlantafx.base.theme.PrimerDark;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
@@ -10,6 +11,7 @@ import java.io.IOException;
 public class MainApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
+        Application.setUserAgentStylesheet(new PrimerDark().getUserAgentStylesheet());
         FXMLLoader fxmlLoader = new FXMLLoader(MainApplication.class.getResource("main.fxml"));
         Scene scene = new Scene(fxmlLoader.load(), 700, 500);
         stage.setTitle("Calculadora!");
--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -5,6 +5,7 @@ module german.calculadora1 {
     requires org.controlsfx.controls;
     requires org.kordamp.bootstrapfx.core;
     requires exp4j;
+    requires atlantafx.base;
 
     opens german.calculadora1 to javafx.fxml;
     exports german.calculadora1;
